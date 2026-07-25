ac637a0696a6a86c870dadd6921bbc53aded7d76
Ayyan1605 <jamdarayyan@gmail.com>
2026-07-25 20:55:58 +0530

    Added Array_object.js

---
+++ Arrey_object.js
@@ -0,0 +1,20 @@
+const fruit = ['Apple', 'Banana', 'Orange'];
+
+console.log(fruit[0]);
+
+fruit.push('Mango');
+fruit.unshift('Kiwi');
+fruit.pop();
+
+const total = fruit.length;
+console.log(total);
+
+// Object
+const user = {
+    name: "HET",    
+    age: "33",
+    isAdmin: true
+};
+
+console.log(user.name);
+console.log(user['age']);
