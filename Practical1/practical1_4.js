457ecc70a2122cd608a18ff4b37f94eec5e625d7
Ayyan1605 <jamdarayyan@gmail.com>
2026-08-04 21:26:10 +0530

    Add Practical1_4.js

---
+++ Practical1/practical1_4.js
@@ -0,0 +1,9 @@
+let firstName = "Aman";
+let lastName = "Gupta";
+
+// Function
+function greet(fname, lname) {
+    return "Hello " + fname + " " + lname;
+}
+
+console.log(greet(firstName, lastName));
